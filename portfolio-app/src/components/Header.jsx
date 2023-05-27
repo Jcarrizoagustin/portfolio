c9053079c0437ed10c4b7f9bd5e70525988d6950
Agustin Carrizo <jcarrizoagustin@gmail.com>
2023-05-26 21:27:34 -0300

    Menu responsive y correccion margin en footer

--- portfolio-app/src/components/Header.jsx
+++ portfolio-app/src/components/Header.jsx
@@ -1,26 +1,45 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
+import { useRef } from "react";
 
 function Header() {
+  const refMenu = useRef();
+
+  const menuHandler = () => {
+    refMenu.current.classList.toggle(styles.showMenu);
+  };
+
   return (
     <div className={styles.header}>
-      <nav className={styles.menu}>
+      <nav className={styles.menu} ref={refMenu}>
         <li className={styles.link}>
-          <Link className={styles.linkRouter} to="/">
+          <Link onClick={menuHandler} className={styles.linkRouter} to="/">
             Sobre mi
           </Link>
         </li>
         <li className={styles.link}>
-          <Link className={styles.linkRouter} to="/proyectos">
+          <Link
+            onClick={menuHandler}
+            className={styles.linkRouter}
+            to="/proyectos"
+          >
             Proyectos
           </Link>
         </li>
         <li className={styles.link}>
-          <Link className={styles.linkRouter} to="/contacto">
+          <Link
+            onClick={menuHandler}
+            className={styles.linkRouter}
+            to="/contacto"
+          >
             Contacto
           </Link>
         </li>
       </nav>
+      <div className={styles.menuIcon} onClick={menuHandler}>
+        <FiMenu size={"36px"} />
+      </div>
     </div>
   );
 }
