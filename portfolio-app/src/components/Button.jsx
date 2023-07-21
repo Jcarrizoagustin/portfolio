f26192f280e6e89fd68e7027d0f6f1cda514dcaa
Agustin Carrizo <jcarrizoagustin@gmail.com>
2023-07-21 19:51:59 -0300

    refactor: change CV file

--- portfolio-app/src/components/Button.jsx
+++ portfolio-app/src/components/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 function Button({ text }) {
   return (
-    <a href="/Agustin_Carrizo-CV.pdf" download className={styles.button}>
+    <a href="/AgustinCarrizo_CV.pdf" download className={styles.button}>
       {text}
     </a>
   );
